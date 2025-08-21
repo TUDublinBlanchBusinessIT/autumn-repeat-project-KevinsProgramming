589e182f23110324089127eb7a3bda787d23788a
KevinsProgramming <B00157434@mytudublin.ie>
2025-08-21 17:50:34 +0100

    {SSWDR} - DBcon.php

--- SSWDR/DBcon.php
+++ SSWDR/DBcon.php
@@ -10,4 +10,9 @@ date_default_timezone_set('Europe/Dublin');
 // Creating the connection to the database
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
