7845caeec546790e826098a458d2e4acbf954137
KevinsProgramming <B00157434@mytudublin.ie>
2025-08-21 17:11:47 +0100

    {SSWDR} - DBcon.php changes

--- SSWDR/DBcon.php
+++ SSWDR/DBcon.php
@@ -1,2 +1,13 @@
-Contact list 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "GymMembership";
+$port = 3307;
+
+date_default_timezone_set('Europe/Dublin');
+
+// Creating the connection to the database
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
 
