03a93465f4783efa151f7be2b6e66b64317480d1
KevinsProgramming <B00157434@mytudublin.ie>
2025-08-26 18:05:33 +0100

    {SSWDR} - delete contact php coding

---
+++ SSWDR/delete_contact.php
@@ -0,0 +1,11 @@
+<?php
+require 'dbcon.php';
+$id = $_GET['id'];
+$sql = "DELETE FROM `contacts` WHERE id = $id";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    header("Location: index.php?msg=Record deleted successfully");
+} else {
+    echo "Failed: " . mysqli_error($conn);
+}
