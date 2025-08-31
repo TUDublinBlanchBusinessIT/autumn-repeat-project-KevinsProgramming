c75397ed63d2f0496194e933d9375441bdef99a2
KevinsProgramming <B00157434@mytudublin.ie>
2025-08-31 22:29:35 +0100

    {WAF} - web.php edit + testing connection

--- WAF/routes/web.php
+++ WAF/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::resource('authors', AuthorController::class);
+Route::resource('books', BookController::class);
