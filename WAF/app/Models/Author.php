2d341f7132ce0fa789edd3b80ea9c1fd0dea2813
KevinsProgramming <B00157434@mytudublin.ie>
2025-09-01 01:42:07 +0100

    {WAF} - New

--- WAF/app/Models/Author.php
+++ WAF/app/Models/Author.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['name', 'email', 'bio'];
+    
+    public function books() { 
+        return $this->hasMany(Book::class); 
+    }
 }
--- WAF/app/Models/Book.php
+++ WAF/app/Models/Book.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['author_id', 'title', 'published_year', 'pages'];
+    
+    public function author() { 
+        return $this->belongsTo(Author::class); 
+    }
 }
